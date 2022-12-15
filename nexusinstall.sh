be9696049be73ba8c68dd9b8c9f813cb70f1b49c
Rohit <singirikonda.rohith@gmail.com>
2022-12-15 23:07:56 +0000

    Nexus Installation File

--- nexusinstall.sh
+++ nexusinstall.sh
@@ -1,19 +1,29 @@
-#!/bin/bash
+docker run -d -p 8081:8081 --name nexus sonatype/nexus3
 
-sudo su
+##########################################
+#To get the Admin password
+#docker container exec nexus cat /nexus-data/admin.password
+
+####################################################################################*
+
+<<COMMENT
+#!/bin/bash
 sudo apt update -y
-sudo apt install openjdk-11-jre-headless -y
+sudo apt install openjdk-8-jre-headless -y
 sudo java -version
 sleep 5
 sudo apt-get install wget
-sudo wget https://download.sonatype.com/nexus/3/nexus-3.44.0-01-unix.tar.gz
-sudo tar -xvf nexus-3.44.0-01-unix.tar.gz
-sudo cd nexus-3.44.0-01/bin
-./nexus start #( starts the nexus artifactory )
-./nexus status #( by this you check the status of nexus artifactory )
+cd /home/ubuntu/
+wget https://download.sonatype.com/nexus/3/nexus-3.44.0-01-unix.tar.gz
+tar -xvf nexus-3.44.0-01-unix.tar.gz
+./nexus-3.44.0-01/bin/nexus start #( starts the nexus artifactory )
+./nexus-3.44.0-01/bin/nexus status #( by this you check the status of nexus artifactory )
 
+COMMENT
 
 #To access this use http://ip_Address:8081 ( by deafault which will be running on 8081)
 #intial password will be present in /opt/sonatype-work/nexus3/admin.password
 
+
 #log ------> sudo tail -f /var/log/cloud-init-output.log
+#sudo update-alternatives --config java
